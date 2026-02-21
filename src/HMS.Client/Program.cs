ebc65d93ca51bcc42f30e61400b94dc3feba2cee
James <mj3sap2con@gmail.com>
2026-02-21 14:40:49 -0500

    small fixes for website

--- src/HMS.Client/Program.cs
+++ src/HMS.Client/Program.cs
@@ -1,5 +1,10 @@
+using HMS.Infrastructure.Extensions;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
 await builder.Build().RunAsync();
+
+
